d8882aff3ab28322048b7879b13b216693fb2560
steedn <steedn@outlook.com>
2021-05-07 09:36:26 -0600

    started styling and planning

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
+import '../styles/image1.css'
+
 const Home = () =>{
 
 
     return(
         <div>
-            <h1> This is testing home</h1>
-            <a href='/Work'>Work</a>
+            <p className='heading1'>My portfolio</p>
+            <a className='button1' href='/Work'>Work</a>
             {/* <a href='/project1'>Project1</a><br></br>
             <a href='/project2'>Project2</a><br></br>
             <a href='/project3'>Project3</a><br></br>
--- src/containers/WorkContainer.js
+++ src/containers/WorkContainer.js
@@ -1,5 +1,7 @@
 import Project1 from '../components/Project1'
 import { BrowserRouter as Link, Route} from 'react-router-dom';
+import Air from '../images/air1.jpg';
+// import img from '../styles/image1.css';
 
 
 const WorkContainer = (match) => {
@@ -7,11 +9,12 @@ const WorkContainer = (match) => {
     return(
         <div>
         {/* <Link to='/'>Home</Link> */}
-        <a href={`NathanSteed/Project1`}>Project1</a><br></br>
+        <img className="img" src={Air}></img>
+        {/* <a href={`NathanSteed/Project1`}>Project1</a><br></br>
         <a href='NathanSteed/project2'>Project2</a><br></br>
         <a href='NathanSteed/project3'>Project3</a><br></br>
         <a href='NathanSteed/project4'>Project4</a><br></br>
-        <a href='NathanSteed/project5'>Project5</a><br></br>
+        <a href='NathanSteed/project5'>Project5</a><br></br> */}
       </div>
     )
 }
