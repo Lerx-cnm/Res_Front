73063ceb2715a928927fe0bd6aefe30e2733b2ab
steedn <steedn@outlook.com>
2021-05-18 20:11:37 -0600

    newer push

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,17 @@
 import '../styles/image1.css'
-
+import { Link } from 'react-router-dom';
+// import Stair from '../images/stair.jpg';
+const text= 'I am a current student attending university for Computer Engineering with a passion for Software Development.\n\n lorem ispumajsdkfljasdklfjaksldfjkalsdjfklajsdfklajsdklf;ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffddddddddddddddddddddddd'
 const Home = () =>{
-
+    
 
     return(
         <div>
-            <p className='heading1'>My portfolio</p>
-            <a className='button1' href='/Work'>Work</a>
+            {/* <img className="img" src={Stair}></img> */}
+            <p className='heading1' style={{ textAlign: 'center'}}>I am Nathan, welcome to my portfolio.</p>
+            <p style={{color: 'white', fontSize: '18px', textAlign: 'center'}}>{text}</p>
+            <Link className='button1' to='/Work'>My Work</Link>
+            {/* <a className='button1' href='/Work'>Work</a> */}
             {/* <a href='/project1'>Project1</a><br></br>
             <a href='/project2'>Project2</a><br></br>
             <a href='/project3'>Project3</a><br></br>
